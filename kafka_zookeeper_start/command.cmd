d1cac8a4aa0d40d092933d0638a7e50edd1312a1
RashmiKuliyal <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-11-24 12:52:37 +0530

    Create command.cmd

---
+++ kafka_zookeeper_start/command.cmd
@@ -0,0 +1,6 @@
+// to start zookeeper server (i am usign windows so these are commands for windows for other you just have to exclude windows subfolder and use.sh file insteam of bat file)
+bin\windows\zookeeper-server-start.bat config\zookeeper.properties
+
+
+// in another terminal start kafka server
+bin\windows\kafka-server-start.bat config\server.porperties
